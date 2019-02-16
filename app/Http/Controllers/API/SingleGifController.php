97dfb44e223d964762303f8351efb13ab3fe1c90
Chris Sevilleja <chris@scotch.io>
2019-02-15 20:43:44 -0800

    adding single gif endpoint

---
+++ app/Http/Controllers/API/SingleGifController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SingleGifController extends Controller
+{
+    public function __invoke(string $id)
+    {
+        $giphyKey = config('services.giphy.key');
+        $url = "https://api.giphy.com/v1/gifs/{$id}?api_key={$giphyKey}";
+
+        $client = new Client();
+        $response = $client->get($url);
+        $gif = json_decode($response->getBody());
+
+        return format_gif($gif);
+    }
+}
